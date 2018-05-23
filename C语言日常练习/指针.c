bdc9a38bf1eadfc45b0a672b61f8213712f4dfcf
SC1996 <846142495@qq.com>
2018-05-23 14:23:27 +0800

    Update 指针.c

--- C语言日常练习/指针.c
+++ C语言日常练习/指针.c
@@ -5,7 +5,7 @@ int main()
   int a,b,*p1,*p2,*p;
   printf("please enter the value of a and b\n");
   scanf("%d",&a);
-  scanf("%d",&b);
+  scanf("%d",&b);//scanf()函数在实际使用时会有很多问题
 
   p1=&a;
   p2=&b;
