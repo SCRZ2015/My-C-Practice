cf11c1d0b2194f83004c36a47f9de61d5afdfdd0
SC1996 <846142495@qq.com>
2018-07-09 21:49:42 +0800

    Add files via upload

---
+++ Ch01/inform.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    char fname[40];
+    char lname[40];
+
+    printf("Enter your first name:\n");
+    scanf("%s\n",fname);
+    printf("Enter your last name:\n");
+    scanf("%s",lname);
+
+    printf("");
+
+    return 0;
+}
